d3ceabfc3857a738f305a24c95c179c72e1e57fa
Saeem Osman <saeem.osman@gmail.com>
2019-02-06 16:21:12 +0600

    navigation and react-router-dom basic operation completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,26 @@
 import React from 'react'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
+import StreamList from './streams/StreamList'
+import StreamCreate from './streams/StreamCreate'
+import StreamDelete from './streams/StreamDelete'
+import StreamEdit from './streams/StreamEdit'
+import StreamShow from './streams/StreamShow'
+import Header from './Header'
 
-const PageOne = () =>{
-    return(
-        <div>
-            This is from pageOne. <br />
-            <button><Link to="/pagetwo">ClickMe </Link></button>
-        </div>
-    )
-}
 
-const PageTwo = () =>{
-    return(
-        <div>
-            This is a nice little page Two.
-            <br />
-            <button><Link to="/">ClickMe </Link></button>
-            
-        </div>
-    )
-}
+
 const App = () =>{
     return(
-        <div>
+        <div className="ui container">
+            
             <BrowserRouter>
                 <div>
-                    <Route path='/' exact component={PageOne} />
-                    <Route path='/pagetwo' component={PageTwo} />
+                <Header />
+                    <Route path="/" exact component = {StreamList} />
+                    <Route path="/streams/edit" exact component = {StreamEdit} />
+                    <Route path="/streams/new" exact  component = {StreamCreate} />
+                    <Route path="/streams/delete" exact component = {StreamDelete} />
+                    <Route path="/streams/show" exact component = {StreamShow} />
                 </div>
             
             </BrowserRouter>
