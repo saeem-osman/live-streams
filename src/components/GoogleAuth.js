7f1af432a83a93844da0131bc4a3ce45c9ebe0e3
Saeem Osman <saeem.osman@gmail.com>
2019-02-11 10:09:07 +0600

    google authentication in react part is done

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,17 +1,67 @@
 import React, { Component } from 'react'
 
 class GoogleAuth extends Component {
+    state = {
+        isSignedIn : null
+    }
     componentDidMount(){
-        window.gapi.load('client:auth2',()=>{
+        window.gapi.load('client:auth2', ()=>{
             window.gapi.client.init({
-                clientId: '526972329429-68eo5utq581etgit09lsttnfjadh17je.apps.googleusercontent.com',
-                scope: 'email'
+                clientId : '526972329429-68eo5utq581etgit09lsttnfjadh17je.apps.googleusercontent.com',
+                scope : 'email'
+            }).then(()=>{
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({
+                    isSignedIn : this.auth.isSignedIn.get()
+                })
+                this.auth.isSignedIn.listen(this.onAuthChange)
             })
         })
     }
+
+    onAuthChange = () =>{
+        this.setState({
+            isSignedIn : this.auth.isSignedIn.get()
+        })
+    }
+    onSignedIn = () =>{
+         this.auth.signIn()
+        
+    }
+    onSignedOut = () =>{
+         this.auth.signOut()
+    }
+
+    renderAuthenticationChange(){
+        if(this.state.isSignedIn === null){
+            return null;
+        }
+        else if(this.state.isSignedIn){
+            return(
+                <div>
+                    <button onClick={this.onSignedOut} className="ui goodle red button">
+                        <i className="goodle icon" />
+                        Sign Out
+                    </button>
+                </div>
+            )
+        }else{
+            return(
+                <div>
+                    <button onClick={this.onSignedIn} className="ui google red button">
+                        <i className="google icon" />
+                        Sign In with Google
+                    </button>
+                </div>
+            )
+        }
+    }
+
     render(){
         return(
-            <div>Google Auth</div>
+            <div>
+                {this.renderAuthenticationChange()}
+            </div>
         )
     }
 }
